f4c72dfc979c96390a16c5a8f8b685f1fe441027
Kendrick J. Humphrey <kendrick8596@gmail.com>
2024-04-11 16:23:12 -0500

    About and Contact Info Added

--- screens/ContactScreen.js
+++ screens/ContactScreen.js
@@ -1,11 +1,19 @@
-import { FlatList, ScrollView } from "react-native";
-import { Avatar, ListItem } from "react-native-elements";
-import { useState } from "react";
-import { CAMPSITES } from '../shared/campsites';
+import { FlatList, ScrollView, Text } from "react-native";
+import { Avatar, ListItem, Card } from "react-native-elements";
 
 const ContactScreen = () => {
     return (
-        <ScrollView></ScrollView>
+        <ScrollView>
+            <Card wrapperStyle={{margin: 20}}>
+                <Card.Title>Contact Information</Card.Title>
+                <Card.Divider />
+                <Text>1 Nucamp Way</Text>
+                <Text>Seattle, WA 98001</Text>
+                <Text style={{ marginBottom: 10 }}>U.S.A.</Text>
+                <Text>Phone: 1-206-555-1234</Text>
+                <Text>Email: campsites@nucamp.co</Text>
+            </Card>
+        </ScrollView>
     )
 };
 
